f3d44ee17b58a222d9148e31f134b09adc24015d
arne-ron <arne.roenneberg@gmail.com>
2026-03-28 13:36:21 +0000

    fixed code

--- src/select-event-preferences.tsx
+++ src/select-event-preferences.tsx
@@ -19,17 +19,18 @@ function SelectEventPreferences() {
     }
 
 
+
     return (
         <div className={'w-full'}>
             <h1>Event Options</h1>
             <p> What events would you like to see? </p>
             <div className={'flex-col space-y-2 p-3'}>
                 {options.map(option =>
-                    <div className={'flex gap-2 bg-gray-200 p-3 rounded-l'}>
+                    <div className={'flex gap-2 bg-gray-200 p-3 rounded-l'} key={option}>
                         <input
                             type="checkbox"
-                            name="tomato"
-                            checked={inputs[option]}
+                            name={option}
+                            checked={inputs[option] || false}
                             onChange={handleChange}
                         />
                         <p>{option}</p>
@@ -37,8 +38,11 @@ function SelectEventPreferences() {
                 )}
                 <p>Make your own suggestion</p>
                 <div className={'flex gap-2'}>
-                    <input onChange={(e) => setSuggestion(e.value)} type={'text'} placeholder={"E.g. Portobello Campfire"} value={suggestion} className={'grow bg-gray-50 rounded-s px-2'}/>
-                    <button type={"button"} onClick={() => {setOptions(options => ({...options, suggestion}))}} className={'bg-green-100 p-1 rounded-s'}>
+                    <input onChange={(e) => setSuggestion(e.target.value)} type={'text'} placeholder={"E.g. Portobello Campfire"} value={suggestion} className={'grow bg-gray-50 rounded-s px-2'}/>
+                    <button type={"button"} onClick={() => {
+                        setOptions(options => ([...options, suggestion]));
+                        setSuggestion("")
+                    }} className={'bg-green-100 p-1 rounded-s'}>
                         <p>Submit</p>
                     </button>
                 </div>
